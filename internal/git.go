9a1bdf7f6f0fa6eb4d0e2d62cbc5c40128208a54
Kieran Osgood <kieran.osgood@and.digital>
2022-08-23 08:05:28 +0100

    refactor: remove unused commands - tidy up error handling

--- internal/git.go
+++ internal/git.go
@@ -2,38 +2,16 @@ package internal
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/go-git/go-git/v5/config"
 	"io"
-	"log"
 	"os"
 
 	"github.com/go-git/go-billy/v5/memfs"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
 func GetRepository(url string, publicKeys *ssh.PublicKeys) (*git.Repository, error) {
-	//home, _ := os.UserHomeDir()
-	//// Open file on disk.
-	//f, _ := os.Open(home + "/.ssh/update-git-tags_circle-ci")
-	//
-	//// Read entire JPG into byte slice.
-	//reader := bufio.NewReader(f)
-	//content, _ := ioutil.ReadAll(reader)
-	//
-	//// Encode as base64.
-	//encoded := b64.StdEncoding.EncodeToString(content)
-	//PrintInfo(encoded)
-
-	//err = os.RemoveAll("bin/repo")
-	//r, err := git.PlainClone("bin/repo", false, &git.CloneOptions{
-	//	URL:      *Flags.Project_url,
-	//	Auth:     publicKeys,
-	//	Progress: os.Stdout,
-	//})
 	r, err := git.Clone(memory.NewStorage(), memfs.New(), &git.CloneOptions{
 		URL:      url,
 		Auth:     publicKeys,
@@ -44,33 +22,6 @@ func GetRepository(url string, publicKeys *ssh.PublicKeys) (*git.Repository, err
 	}
 
 	return r, nil
-}
-
-func GetTags(r *git.Repository) (Tags []string, Error error) {
-	t, err := r.TagObjects()
-	if err != nil {
-		return nil, err
-	}
-
-	// Retrieves all tags for current Branch
-	var tags []string
-	err = t.ForEach(func(t *object.Tag) error {
-		tags = append(tags, t.Name)
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// Retrieves *all* tags
-	//tags, err := r.Tags()
-	//err = tags.ForEach(func(tag *plumbing.Reference) error {
-	//	PrintInfo("tag.Name: %v\n", tag.Name())
-	//	PrintInfo("tag.Type: %v \n\n", tag.Type())
-	//	return nil
-	//})
-
-	return tags, nil
 }
 
 func ReadJson(w *git.Worktree, path string) (*[]byte, error) {
@@ -96,37 +47,10 @@ func ReadJson(w *git.Worktree, path string) (*[]byte, error) {
 	return &b, nil
 }
 
-func tagExists(tag string, r *git.Repository) bool {
-	tagFoundErr := "tag was found"
-	PrintInfo("git show-ref --tag")
-	tags, err := r.TagObjects()
-	if err != nil {
-		log.Printf("get tags error: %s", err)
-		return false
-	}
-	res := false
-	err = tags.ForEach(func(t *object.Tag) error {
-		if t.Name == tag {
-			res = true
-			return fmt.Errorf(tagFoundErr)
-		}
-		return nil
-	})
-	if err != nil && err.Error() != tagFoundErr {
-		PrintInfo("iterate tags error: %s", err)
-		return false
-	}
-	return res
-}
-
 func CreateTag(r *git.Repository, tagName string) (Success bool, Error error) {
-	if tagExists(tagName, r) {
-		PrintWarning("tagName %s already exists", tagName)
-		return false, nil
-	}
 	h, err := r.Head()
 	if err != nil {
-		PrintWarning("get HEAD error: %s", err)
+		PrintWarning("r.HEAD: %w", err)
 		return false, err
 	}
 	PrintInfo("Set tagName %s", tagName)
@@ -140,7 +64,7 @@ func CreateTag(r *git.Repository, tagName string) (Success bool, Error error) {
 	)
 
 	if err != nil {
-		PrintError("CreateTag error: %s", err)
+		PrintError("CreateTag: %w", err)
 		return false, err
 	}
 
@@ -158,12 +82,14 @@ func PushTags(r *git.Repository, publicKeys *ssh.PublicKeys) error {
 	err := r.Push(po)
 
 	if err != nil {
-		if err == git.NoErrAlreadyUpToDate {
+		switch err {
+		case git.NoErrAlreadyUpToDate:
 			PrintInfo("origin remote was up to date, no push done")
 			return nil
+		default:
+			PrintInfo("push to remote origin error: %w", err)
+			return err
 		}
-		PrintInfo("push to remote origin error: %s", err)
-		return err
 	}
 
 	return nil
